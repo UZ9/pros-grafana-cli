3995e69435b19891d9db21ade4ad17361e3eaba5
Yerti <chessboy5@icloud.com>
2022-06-12 18:17:36 -0500

    :bug: Fix setup.py not finding install_requires module during pip install

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-# setup.py for non-frozen builds
-
 from setuptools import setup, find_packages
-from install_requires import install_requires as install_reqs
 
 setup(
     name='pros-grafana-cli',
     version=open('pip_version').read().strip(),
     packages=find_packages(),
-    url='https://github.com/purduesigbots/pros-grafana-cli',
+    url='https://github.com/UZ9/pros-grafana-cli',
     license='MPL-2.0',
     author='Yerti',
-    # author_email='',
     description='Command Line Interface for interacting with Grafana through a V5 Brain.',
-    install_requires=install_reqs,
+    install_requires=[
+        "click>=6,<7",
+        "pros-cli"
+        "setuptools~=56.0.0"
+    ],
     entry_points={
         'console_scripts': [
             'prosgrafana=prosgrafana.cli.gui:gui',
